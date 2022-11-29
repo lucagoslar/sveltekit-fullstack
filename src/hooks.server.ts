1ddba77864400acedbf57b85b8533ba49b37bafa
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2022-11-29 14:01:21 +0100

    feat: bump pkg versions

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,26 @@
+import { sequence } from '@sveltejs/kit/hooks';
 import { isLocale } from '$i18n/i18n-util';
 import type { Handle } from '@sveltejs/kit';
-import { createTRPCHandle } from '$lib/trpc/hook';
+import { createTRPCHandle } from 'trpc-sveltekit';
 import { router } from '$lib/trpc/server';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	// Extract language
+const language: Handle = async ({ event, resolve }) => {
 	let [, lang] = event.url.pathname.split('/');
 
 	if (isLocale(lang)) {
 		// Set lang attribute
-		return resolve(event, {
+		return await resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%lang%', lang)
 		});
 	}
 
-	const response = await createTRPCHandle({
-		url: '/trpc',
-		router,
-		createContext: async () => ({}),
-		event,
-		resolve
-	});
-
-	return response;
+	return await resolve(event);
 };
+
+const trpc: Handle = createTRPCHandle({
+	url: '/trpc',
+	router,
+	createContext: async () => ({})
+});
+
+export const handle: Handle = sequence(language, trpc);
