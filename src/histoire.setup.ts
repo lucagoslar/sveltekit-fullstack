57c307e83825bde8bd16e342b9eb26a5b58b03ff
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2022-12-01 15:25:51 +0100

    feat(histoire): init i18n

--- src/histoire.setup.ts
+++ src/histoire.setup.ts
@@ -1,2 +1,7 @@
+import { setLocale } from '$i18n/i18n-svelte';
+import { loadLocale } from '$i18n/i18n-util.sync';
 import '$scss/main.scss';
 import 'normalize.css';
+
+loadLocale('de');
+setLocale('de');
