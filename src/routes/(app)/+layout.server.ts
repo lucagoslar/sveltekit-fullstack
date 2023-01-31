df72f1b6fddaa083660eb79288df06f8fb7f34a5
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2023-01-31 23:33:46 +0100

    refactor(normalise): req to event

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -6,12 +6,12 @@ import parser from 'accept-language-parser';
 import { baseLocale, locales } from '$i18n/i18n-util';
 import type { Locales } from '$i18n/i18n-types';
 
-export const load: LayoutServerLoad = async (req) => {
-	const browserLanguage = parser.pick(locales, req.request.headers.get('accept-language') || '');
-	const locale = req.params.locale as Locales;
+export const load: LayoutServerLoad = async (event) => {
+	const browserLanguage = parser.pick(locales, event.request.headers.get('accept-language') || '');
+	const locale = event.params.locale as Locales;
 
 	if (!locale || !locales.includes(locale)) {
-		let pathname = req.url.pathname.split('/').filter(Boolean);
+		let pathname = event.url.pathname.split('/').filter(Boolean);
 		pathname.unshift((browserLanguage as string) || baseLocale);
 
 		throw redirect(308, pathname.join('/'));
