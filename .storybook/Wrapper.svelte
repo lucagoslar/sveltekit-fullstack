35ca19e33cf985acda5737236dcf40bf1b67891c
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2023-04-07 17:47:34 +0200

    feat: storybook integration
    BREAKING CHANGE:

---
+++ .storybook/Wrapper.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { locales } from '$i18n/i18n-util';
+	import Layout from '../src/routes/(app)/+layout.svelte';
+
+	let data = { locale: locales[0] };
+</script>
+
+<Layout {data}><slot /></Layout>
---
+++ src/lib/components/atoms/Logo/Logo.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+	import Logo from './Logo.svelte';
+</script>
+
+<Meta title="Atoms/Logo" component={Logo} />
+
+<Template let:args>
+	<Logo {...args} />
+</Template>
+
+<Story name="Default" />
