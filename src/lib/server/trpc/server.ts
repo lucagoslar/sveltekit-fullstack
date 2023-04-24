45954e1a247c366b8504c43eacdd4182bd87c9bb
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2023-04-24 20:38:41 +0200

    fix: error handler

--- src/lib/server/trpc/server.ts
+++ src/lib/server/trpc/server.ts
@@ -1,15 +1,23 @@
 import { initTRPC, TRPCError } from '@trpc/server';
 import type { Context } from './createContext';
 import { isAuthenticated } from '$lib/server/trpc/middleware/isAuthenticated';
+import { ZodError } from 'zod';
 
 export const t = initTRPC.context<Context>().create({
-	// errorFormatter: ({ shape, error, type, path, input, ctx }) => {
-	// 	console.log('tRPC error:', JSON.stringify({ shape, error, type, path, input, ctx }));
-	// 	return {
-	// 		code: shape.code,
-	// 		message: shape.message
-	// 	};
-	// }
+	errorFormatter({ shape, error }) {
+		console.log(error);
+
+		return {
+			...shape,
+			data: {
+				...shape.data,
+				zodError:
+					error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
+						? error.cause.flatten()
+						: null
+			}
+		};
+	}
 });
 
 export const router = t.router;
